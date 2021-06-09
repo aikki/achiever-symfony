ecd08c1c8a8d6dd5aa5babfd968cb39566234303
Patryk Hołderny <akitaki0@gmail.com>
2021-06-09 23:22:42 +0200

    Club forgotten use statement

--- src/Entity/Club.php
+++ src/Entity/Club.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use App\Repository\ClubRepository;
 
 /**
  * @ORM\Entity(repositoryClass=ClubRepository::class)
